11e891e16345f6bb86f8e9b155481fe01390793c
Cyan <2657031304@qq.com>
2025-04-17 17:17:13 +0800

    [frontend] 样式类名修改

--- app-engine/frontend/src/pages/appDetail/index.tsx
+++ app-engine/frontend/src/pages/appDetail/index.tsx
@@ -49,7 +49,7 @@ const AppDetail: React.FC = () => {
           <GoBack path={'/app-develop'} title={t('appDetail')} />
         </div>
       </div>
-      <div className='aui-block appenine-detail' style={{ paddingTop: 0 }}>
+      <div className='aui-block appengine-detail' style={{ paddingTop: 0 }}>
         <Tabs type='card' activeKey={tabsKey} items={items} onChange={onChange} />
         <div />
       </div>
