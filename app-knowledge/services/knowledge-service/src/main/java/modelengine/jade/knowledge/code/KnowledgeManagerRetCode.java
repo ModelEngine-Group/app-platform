a9cf9507dad55878d38b83515abcfb6bdfa590a4
陈潇文 <chenxiaowen17@huawei.com>
2025-04-29 18:56:07 +0800

    [frontend] 对qianfan接口异常报错区分处理

--- app-knowledge/services/knowledge-service/src/main/java/modelengine/jade/knowledge/code/KnowledgeManagerRetCode.java
+++ app-knowledge/services/knowledge-service/src/main/java/modelengine/jade/knowledge/code/KnowledgeManagerRetCode.java
@@ -19,10 +19,37 @@ public enum KnowledgeManagerRetCode implements RetCode {
      * 查询到默认使用的config数量超过一个。
      */
     QUERY_CONFIG_LENGTH_MORE_THAN_ONE(130703002, "在{0}知识库平台有超过1个配置为默认使用，请检查知识库配置。"),
+
     /**
      * 查询知识库列表接口失败。
      */
-    QUERY_KNOWLEDGE_LIST_ERROR(130703003, "获取知识库列表失败，请检查api key配置。");
+    QUERY_KNOWLEDGE_LIST_ERROR(130703003, "获取知识库列表失败，原因：{}。"),
+
+    /**
+     * 请求知识库平台鉴权失败
+     */
+    AUTHENTICATION_ERROR(130703004, "知识库平台鉴权失败，原因：{}。"),
+
+    /**
+     * 请求知识库平台内部失败
+     */
+    INTERNAL_SERVICE_ERROR(130703005, "知识库平台接口内部错误，原因：{}。"),
+
+    /**
+     * 客户端请求错误
+     */
+    CLIENT_REQUEST_ERROR(130703006, "知识库平台请求错误，原因：{}。"),
+
+    /**
+     * 资源不存在
+     */
+    NOT_FOUND(130703007, "知识库平台接口不存在，原因：{}。"),
+
+    /**
+     * 知识库检索失败
+     */
+    QUERY_KNOWLEDGE_ERROR(130703008, "知识库检索失败，原因：{}。");
+
     private final int code;
     private final String msg;
 
