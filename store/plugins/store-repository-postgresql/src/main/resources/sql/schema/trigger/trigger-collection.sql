4c3cc9b3543a32c6188a2a20797492031e3a9d73
Muggle <125979968+jsbjfkbsjk@users.noreply.github.com>
2025-07-22 10:37:46 +0800

    Update trigger-collection.sql

--- store/plugins/store-repository-postgresql/src/main/resources/sql/schema/trigger/trigger-collection.sql
+++ store/plugins/store-repository-postgresql/src/main/resources/sql/schema/trigger/trigger-collection.sql
@@ -1,5 +1,5 @@
 drop trigger if exists trigger_update_collection ON store_collection;
-create  trigger trigger_update_collection
+create trigger trigger_update_collection
     before update ON store_collection
     for each row
 execute function update_when_update();
