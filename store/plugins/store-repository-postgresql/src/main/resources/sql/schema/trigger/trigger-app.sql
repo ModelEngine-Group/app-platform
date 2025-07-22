9a34cf45a48221b825e05d1becfb0e775202ba16
Muggle <125979968+jsbjfkbsjk@users.noreply.github.com>
2025-07-22 10:36:56 +0800

    Update trigger-app.sql

--- store/plugins/store-repository-postgresql/src/main/resources/sql/schema/trigger/trigger-app.sql
+++ store/plugins/store-repository-postgresql/src/main/resources/sql/schema/trigger/trigger-app.sql
@@ -1,5 +1,5 @@
 drop trigger if exists trigger_update_app ON store_app;
-create  trigger trigger_update_app
+create trigger trigger_update_app
     before update ON store_app
     for each row
 execute function update_when_update();
