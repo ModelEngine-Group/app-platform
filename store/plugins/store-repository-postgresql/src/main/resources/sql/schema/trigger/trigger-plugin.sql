948143b49664bdba5451469d29e1f140ac7cb35f
Muggle <125979968+jsbjfkbsjk@users.noreply.github.com>
2025-07-22 10:38:37 +0800

    Update trigger-plugin.sql

--- store/plugins/store-repository-postgresql/src/main/resources/sql/schema/trigger/trigger-plugin.sql
+++ store/plugins/store-repository-postgresql/src/main/resources/sql/schema/trigger/trigger-plugin.sql
@@ -1,5 +1,5 @@
 drop trigger if exists trigger_update_plugin ON store_plugin;
-create  trigger trigger_update_plugin
+create trigger trigger_update_plugin
     before update ON store_plugin
     for each row
 execute function update_when_update();
