c17728b199d3047f28e9e4cf838ea63214d67a1b
Muggle <125979968+jsbjfkbsjk@users.noreply.github.com>
2025-07-22 10:39:24 +0800

    Update trigger-task.sql

--- store/plugins/store-repository-postgresql/src/main/resources/sql/schema/trigger/trigger-task.sql
+++ store/plugins/store-repository-postgresql/src/main/resources/sql/schema/trigger/trigger-task.sql
@@ -1,5 +1,5 @@
 drop trigger if exists trigger_update_task ON store_task;
-create  trigger trigger_update_task
+create trigger trigger_update_task
     before update ON store_task
     for each row
 execute function update_when_update();
