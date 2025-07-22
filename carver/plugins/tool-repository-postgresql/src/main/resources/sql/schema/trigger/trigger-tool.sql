550f3390330e6a73ae3f12ed42ee18fd34c78f6f
Muggle <125979968+jsbjfkbsjk@users.noreply.github.com>
2025-07-22 10:36:22 +0800

    Update trigger-tool.sql

--- carver/plugins/tool-repository-postgresql/src/main/resources/sql/schema/trigger/trigger-tool.sql
+++ carver/plugins/tool-repository-postgresql/src/main/resources/sql/schema/trigger/trigger-tool.sql
@@ -1,5 +1,5 @@
-drop trigger  if exists trigger_update_tool ON store_tool;
-create  trigger trigger_update_tool
+drop trigger if exists trigger_update_tool ON store_tool;
+create trigger trigger_update_tool
     before update ON store_tool
     for each row
 execute function update_when_update();
