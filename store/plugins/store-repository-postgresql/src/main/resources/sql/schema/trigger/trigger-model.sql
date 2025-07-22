f7c802b45a7e6063f6a57c6858ca45f62f6a5cb5
Muggle <125979968+jsbjfkbsjk@users.noreply.github.com>
2025-07-22 10:37:16 +0800

    Update trigger-model.sql

--- store/plugins/store-repository-postgresql/src/main/resources/sql/schema/trigger/trigger-model.sql
+++ store/plugins/store-repository-postgresql/src/main/resources/sql/schema/trigger/trigger-model.sql
@@ -1,5 +1,5 @@
 drop trigger if exists trigger_update_model ON store_model;
-create  trigger trigger_update_model
+create trigger trigger_update_model
     before update ON store_model
     for each row
 execute function update_when_update();
