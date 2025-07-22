62b111650e3e0df4ffe1a34d32fd76c2902fb239
Muggle <125979968+jsbjfkbsjk@users.noreply.github.com>
2025-07-22 10:38:07 +0800

    Update trigger-plugin-tool.sql

--- store/plugins/store-repository-postgresql/src/main/resources/sql/schema/trigger/trigger-plugin-tool.sql
+++ store/plugins/store-repository-postgresql/src/main/resources/sql/schema/trigger/trigger-plugin-tool.sql
@@ -1,5 +1,5 @@
-drop trigger  if exists trigger_update_plugin_tool ON store_plugin_tool;
-create  trigger trigger_update_plugin_tool
+drop trigger if exists trigger_update_plugin_tool ON store_plugin_tool;
+create trigger trigger_update_plugin_tool
     before update ON store_plugin_tool
     for each row
 execute function update_when_update();
