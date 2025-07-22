9b4b28b06abb56f8d7e6b15afaa971cb5443caf9
Muggle <125979968+jsbjfkbsjk@users.noreply.github.com>
2025-07-22 10:39:01 +0800

    Update trigger-tag.sql

--- store/plugins/store-repository-postgresql/src/main/resources/sql/schema/trigger/trigger-tag.sql
+++ store/plugins/store-repository-postgresql/src/main/resources/sql/schema/trigger/trigger-tag.sql
@@ -1,5 +1,5 @@
 drop trigger if exists trigger_update_tag ON store_tag;
-create  trigger trigger_update_tag
+create trigger trigger_update_tag
     before update ON store_tag
     for each row
 execute function update_when_update();
