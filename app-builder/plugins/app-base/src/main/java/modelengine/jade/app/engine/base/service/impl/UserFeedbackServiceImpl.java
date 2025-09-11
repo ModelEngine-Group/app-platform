d74e3e5153fc11d5aba5f23f0fc448299cfc6448
陈潇文 <chenxiaowen17@huawei.com>
2025-09-11 17:53:25 +0800

    [app-platform] 检视意见修改

--- app-builder/plugins/app-base/src/main/java/modelengine/jade/app/engine/base/service/impl/UserFeedbackServiceImpl.java
+++ app-builder/plugins/app-base/src/main/java/modelengine/jade/app/engine/base/service/impl/UserFeedbackServiceImpl.java
@@ -16,8 +16,9 @@ import modelengine.jade.app.engine.base.mapper.UserFeedbackMapper;
 import java.util.List;
 
 /**
- * Aipp用户反馈功能实现类
+ * Aipp 用户反馈功能实现类
  *
+ * @author 鲁为
  * @since 2024-5-24
  *
  */
