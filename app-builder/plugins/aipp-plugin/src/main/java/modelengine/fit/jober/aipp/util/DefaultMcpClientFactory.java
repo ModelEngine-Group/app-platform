522f16dd5a77ae0d9169c08d39e8ba8fe85e4f23
loveTsong <271667068@qq.com>
2026-03-02 17:19:48 +0800

    [aipp-plugin] docs: add copyright and javadoc for McpClientFactory classes

--- app-builder/plugins/aipp-plugin/src/main/java/modelengine/fit/jober/aipp/util/DefaultMcpClientFactory.java
+++ app-builder/plugins/aipp-plugin/src/main/java/modelengine/fit/jober/aipp/util/DefaultMcpClientFactory.java
@@ -1,7 +1,19 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) 2025 Huawei Technologies Co., Ltd. All rights reserved.
+ *  This file is a part of the ModelEngine Project.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
 package modelengine.fit.jober.aipp.util;
 
 import modelengine.fitframework.annotation.Component;
 
+/**
+ * MCP 客户端工厂的默认实现，使用 {@link LangChain4jMcpClient} 创建客户端实例。
+ *
+ * @author songyongtan
+ * @since 2026-03-02
+ */
 @Component
 public class DefaultMcpClientFactory implements McpClientFactory {
     @Override
--- app-builder/plugins/aipp-plugin/src/main/java/modelengine/fit/jober/aipp/util/McpClientFactory.java
+++ app-builder/plugins/aipp-plugin/src/main/java/modelengine/fit/jober/aipp/util/McpClientFactory.java
@@ -1,8 +1,20 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) 2025 Huawei Technologies Co., Ltd. All rights reserved.
+ *  This file is a part of the ModelEngine Project.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
 package modelengine.fit.jober.aipp.util;
 
-import modelengine.fitframework.annotation.Component;
+import modelengine.fit.jober.aipp.util.LangChain4jMcpClient;
 
 import java.util.function.Function;
 
+/**
+ * MCP 客户端工厂接口，用于创建 {@link LangChain4jMcpClient} 实例。
+ *
+ * @author songyongtan
+ * @since 2026-03-02
+ */
 public interface McpClientFactory extends Function<String, LangChain4jMcpClient> {
 }
