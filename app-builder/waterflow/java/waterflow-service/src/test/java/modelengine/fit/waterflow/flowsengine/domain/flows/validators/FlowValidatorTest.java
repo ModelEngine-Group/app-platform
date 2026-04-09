24f4e7165cb04a409775004d56e3eec036b9346d
qq_62395577 <qq_62395577@noreply.gitcode.com>
2026-04-09 22:28:46 +0800

    修改2

--- app-builder/waterflow/java/waterflow-service/src/test/java/modelengine/fit/waterflow/flowsengine/domain/flows/validators/FlowValidatorTest.java
+++ app-builder/waterflow/java/waterflow-service/src/test/java/modelengine/fit/waterflow/flowsengine/domain/flows/validators/FlowValidatorTest.java
@@ -536,20 +536,6 @@ class FlowValidatorTest extends FlowsDataBaseTest {
             assertEquals(INVALID_START_NODE_EVENT_SIZE.getErrorCode(), exception.getCode());
         }
 
-        @Test
-        @DisplayName("测试流程节点StartNodeEventGreatThan1校验成功")
-        public void testValidateStartNodeEventGreatThan1Success() {
-            Map<String, FlowNode> nodeMap = flowDefinition.getNodeMap();
-            List<FlowEvent> events = new ArrayList<>();
-            events.add(new FlowEvent());
-            events.add(new FlowEvent());
-            nodeMap.get(START_ID).setEvents(events);
-            flowDefinition.setNodeMap(nodeMap);
-
-            WaterflowParamException exception = assertThrows(WaterflowParamException.class,
-                    () -> flowNodeValidator.validate(flowDefinition));
-            assertEquals(INVALID_START_NODE_EVENT_SIZE.getErrorCode(), exception.getCode());
-        }
 
         @Test
         @DisplayName("测试流程节点StartNodeJobber校验成功")
@@ -576,20 +562,6 @@ class FlowValidatorTest extends FlowsDataBaseTest {
             assertEquals(INVALID_STATE_NODE_EVENT_SIZE.getErrorCode(), exception.getCode());
         }
 
-        @Test
-        @DisplayName("测试流程节点StateNodeEventGreatThan1校验成功")
-        public void testValidateStateNodeEventGreatThan1Success() {
-            Map<String, FlowNode> nodeMap = flowDefinition.getNodeMap();
-            List<FlowEvent> events = new ArrayList<>();
-            events.add(new FlowEvent());
-            events.add(new FlowEvent());
-            nodeMap.get(STATE_ID).setEvents(events);
-            flowDefinition.setNodeMap(nodeMap);
-
-            WaterflowParamException exception = assertThrows(WaterflowParamException.class,
-                    () -> flowNodeValidator.validate(flowDefinition));
-            assertEquals(INVALID_STATE_NODE_EVENT_SIZE.getErrorCode(), exception.getCode());
-        }
 
         @Test
         @DisplayName("测试流程节点StateNodeJobber校验成功")
